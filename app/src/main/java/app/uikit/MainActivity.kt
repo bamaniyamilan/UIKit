085a45b2cac13737559b7e5a6d7ab5c54e656f95
bamaniyamilan <bamaniyamilan108@gmail.com>
2022-07-12 14:05:41 +0530

    Milan : splash screen done

--- app/src/main/java/app/uikit/MainActivity.kt
+++ app/src/main/java/app/uikit/MainActivity.kt
@@ -1,12 +1,37 @@
 package app.uikit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        isNetworkConnected()
+        checkInternet()
+    }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun checkInternet() {
+        if (isNetworkConnected() == true)
+            Handler().postDelayed(Runnable {startActivity(Intent(applicationContext, LoginActivity::class.java))}, 2000)
+        else
+            Toast.makeText(applicationContext, "Please Check Internet Connection", LENGTH_LONG).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun isNetworkConnected(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return cm.activeNetworkInfo != null && cm.activeNetworkInfo!!.isConnected
     }
 }
